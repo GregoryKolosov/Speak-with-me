034baa321c5ee874f48c4e5c040f9a1f322efcb8
Gregory <35295599+GregoryKolosov@users.noreply.github.com>
2022-03-24 00:09:16 +0400

    Initial commit

---
+++ MacronWillCall/MacronWillCall/MacronWillCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacronWillCallApp.swift
+//  MacronWillCall
+//
+//  Created by Grigoriy Kolosov on 24.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MacronWillCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
